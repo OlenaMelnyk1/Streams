3f1135949e0a79cd19af280ba678924c57011f1e
eropay_gmail_com <eropay@gmail.com>
2021-08-30 09:02:34 +0300

    qq

--- src/main/java/com/epam/test/automation/java/practice14/low/Task1.java
+++ src/main/java/com/epam/test/automation/java/practice14/low/Task1.java
@@ -3,15 +3,13 @@ package com.epam.test.automation.java.practice14.low;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Task1 {
 
     public static List<String> filterByFirstAndLastCharacter(char symbol, List<String> stringList) {
-        List <String > k = new ArrayList<>();
-    stringList.forEach(
-            list-> {if (list.length()>2 && list.charAt(0)==list.charAt(list.length()-1)&& list.charAt(0)==symbol)
-                k.add(list);
-            });
-        return k;
+        return stringList.stream()
+                .filter(list->(list.length()>2 && list.charAt(0)==list.charAt(list.length()-1)&& list.charAt(0)==symbol))
+                .collect(Collectors.toList());
     }
 }
