29e68699a5108cbc69f12c1353bdffd72dd116ba
eropay_gmail_com <eropay@gmail.com>
2021-08-30 10:09:01 +0300

    2

--- src/main/java/com/epam/test/automation/java/practice14/low/Task2.java
+++ src/main/java/com/epam/test/automation/java/practice14/low/Task2.java
@@ -1,10 +1,15 @@
 package com.epam.test.automation.java.practice14.low;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Task2 {
     public static List<Integer> sortAscLengthsOfStrings(List<String> stringList) {
-        //TODO: Write your own solution in the return statement;
-        return null;
+
+        return stringList.stream()
+                .map(string -> string.length())
+                .sorted()
+                .collect(Collectors.toList());
     }
 }
