02f9f9a3ac4ec37eba20ce40020da200a99ad6e7
eropay_gmail_com <eropay@gmail.com>
2021-09-09 20:08:25 +0300

    level1

--- src/main/java/com/epam/test/automation/java/practice14/middle/Task9.java
+++ src/main/java/com/epam/test/automation/java/practice14/middle/Task9.java
@@ -1,5 +1,7 @@
 package com.epam.test.automation.java.practice14.middle;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Task9 {
@@ -7,4 +9,14 @@ public class Task9 {
     public static List<String> shouldReturnStringListWithLengthAndFirstLetter(List<String> stringList) {
         return null;
     }
+
+   /* public static void main(String[] args) {
+
+        List<Integer> myList = new ArrayList<Integer>(
+                Arrays.asList(-10, 3, -3, 4, 55, 6));
+        //List<Integer > myListIn=new ArrayList<Integer>(
+        //List.of(1, 3, 4));
+        System.out.println(getSortedUnionTwoSubsetsIntegers(3, 4, myList));
+    }*/
 }
+
