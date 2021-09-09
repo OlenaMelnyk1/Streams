9c9cfe66c50a15691d8fb27c130e756b9831fc17
eropay_gmail_com <eropay@gmail.com>
2021-09-09 20:51:45 +0300

    level1

--- src/main/java/com/epam/test/automation/java/practice14/low/Task3.java
+++ src/main/java/com/epam/test/automation/java/practice14/low/Task3.java
@@ -1,11 +1,9 @@
 package com.epam.test.automation.java.practice14.low;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Task3 {
-
     public static List<String> getListExtremeLetters(List<String> list) {
        return list.stream()
                 .map(l->l.substring(0,1)+l.charAt(l.length()-1))
